2ef58288f66bed35abde9676fc5a3ed3a390a9e8
wuyidi@eastmoney.com <wuyidi@eastmoney.com>
2021-01-06 12:51:07 +0800

    page

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import scroll from './scroll/scroll.vue'
-import { App } from 'vue'
-
-scroll.install = (app: App): void => { app.component(scroll.name, scroll) }
-
-export default scroll
--- types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { CScroller } from './scroller.d.ts'
-
-export * from './scroller.d.ts'
-
-export * as CScroller from './scroller.d.ts'
-
-export default CScroller
