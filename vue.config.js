0c33ad23c57a2ba45370a91960a295af575414a4
wuyidi@eastmoney.com <wuyidi@eastmoney.com>
2021-01-06 12:47:45 +0800

    page

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,16 @@ module.exports = {
       .include
       .add(path.join(__dirname, '/src'))
       .end()
+    config.module
+      .rule("md")
+      .test(/\.md$/)
+      .use("vue-loader")
+      .loader("vue-loader")
+      .end()
+      .use("vue-markdown-loader")
+      .loader("vue-markdown-loader")
+      .options({
+        raw: true
+      })
   }
 }
